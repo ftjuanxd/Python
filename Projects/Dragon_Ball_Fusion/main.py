6436d53ec35a384f3efc4906faa26c738332c3b8
Juan Sebastian <davidstivenpinuela@gmail.com>
2024-07-25 14:54:18 -0500

    Dragon_Ball_Fusion Remake V_1

--- Projects/Dragon_Ball_Fusion/main.py
+++
@@ -1,6 +0,0 @@
-import packages.procces as pr
-
-pr.Main()
-
-
-#solo falta crear la opcion funciones
--- Projects/Dragon_Ball_Fusion/packages/procces.py
+++ Projects/Dragon_Ball_Fusion/packages/procces.py
@@ -1,39 +1,30 @@
-from . import Personajes as p
-from . import methods as mt
+from . import design as d
+import tkinter as tk
+
+outputs = ["\nMenu\n1.Create Character\n2.Merger Character\n3.Show Character.\nChoose",["Name","Speed","Power","Energy"],["Name Character 1:","Name Character 2:"]]
+
+mistake = ["Just Numbers"]
+
+root =tk.Tk()
+root.withdraw()
 
 def Main():
-    array = []
-    while True:
-        opc = mt.Menu()
-        if opc == 1:
-            while True:
-                try:
-                    name = input("Type the name of the character: ").lower()
-                    speed = int(input("Type the speed of the character: "))
-                    power = int(input("Type the power of the character: "))
-                    energy = int(input("Type the energy of the character:"))
-                    character = p.Personaje(name,speed,power,energy)
-                    date = [len(array)+1,character,name,speed,power,energy]
-                    array.append(date)
-                    break
-                except ValueError:
-                    print("The speed,power and energy are value numeric") 
-                except:
-                    print("Mistake with the creating of the character")
-        elif opc == 2:
-            char1 = input("Type the name of the first Character: ")            
-            char2 = input("Type the name of the first Character: ")   
-            if char1 in array and char2 in array:
-                print("Logro")         
-            else:
-                print("Nol")
-        elif opc == 3:
-            for i in range(len(array)):
-                print(f"{i+1}.{array[i][2]}.")
-            continue
-        elif opc == 4:
-            print("Gracias Por Jugar. XD")
-            break
-        else:
-            print("La opcion digitada no existe intentelo otra vez. Gracias")
-        mt.clear_Screen()
+    opc = d.Create_Screen(outputs[0])
+    if opc == 1:
+        #getting character's values
+        for values in outputs[1]:
+            d.Create_Screen(values)
+    elif opc == 2:
+        #getting characters' names
+        for values in outputs[2]:
+            d.Create_Screen(values)
+    elif opc == 3:
+        #show characters' names
+        #values would be a list 
+        d.Create_Screen(values)
+    else:
+        #show the mistake and wait five seconds pa
+        d.Create_Screen(mistake[0])
+        d.clear_Screen(3)
+        Main()
+root.mainloop()
